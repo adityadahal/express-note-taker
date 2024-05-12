a41b826d6d911c338bf03babaeab3c367295c572
Aditya Kishor Dahal <toadityadahal@gmail.com>
2024-05-12 18:19:44 +0545

    add: Sequelize added to save data to DB

--- models/NoteModel.js
+++ models/NoteModel.js
@@ -1,12 +1,18 @@
 const sequelize = require('sequelize');
 
-const noteSchema = sequelize.Schema({
-    title: {type: String, required: true},
-    body: {type: String, required: true},
-    user: {type: String, required: true},
+// const noteSchema = sequelize.Schema({
+//     title: {type: String, required: true},
+//     body: {type: String, required: true},
+//     user: {type: String, required: true},
+// })
+
+
+// const noteModel = sequelize.Model("note", noteSchema);
+
+const noteModel = sequelize.define("note", {
+    title:{type: sequelize.DataTypes.STRING, required: true},
+    body:{type: sequelize.DataTypes.STRING, required: true},
+    user:{type: sequelize.DataTypes.STRING, required: true},
 })
-
-
-const noteModel = sequelize.Model("note", noteSchema);
 
 module.exports = noteModel;
