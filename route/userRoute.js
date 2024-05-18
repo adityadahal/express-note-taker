5511f5175e9772ec2c0a6caa27b07b4fcef1a8d9
Aditya Kishor Dahal <toadityadahal@gmail.com>
2024-05-18 11:07:39 +0545

    add: login added

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const userModel = require('../models/UserModel');
 const bycrpt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const crypto = require("crypto")
 
 const userRouter = express.Router();
 
@@ -16,7 +18,6 @@ userRouter.post("/register", async (req, res) => {
         return;
     }
     try {
-        console.log("SUCCESSFUL")
         const hashedPassword = await bycrpt.hash(password, 10);
         let user = new userModel({name, email, password: hashedPassword, username});
         await user.save();
@@ -24,6 +25,26 @@ userRouter.post("/register", async (req, res) => {
         
     } catch (error) {
         res.status(500).json({message: `User with username ${username} cannot be created: `, error})
+    }
+})
+
+userRouter.post("/login", async (req, res)=>{
+    const{email, password} = req.body;
+    try {
+        const user = await userModel.findOne({where: {email}});
+        if(!user){
+            return res.status(401).json({message: "Invalid Email"});
+        }
+        const validPassword = await bycrpt.compare(password, user.password);
+        if(!validPassword){
+            return res.status(401).json({message: "Invalid Password"});
+        }
+        const JWT_SECRET = crypto.randomBytes(64).toString('hex');
+        const token = jwt.sign({id: user.id, email: user.email}, JWT_SECRET, {expiresIn: '1d'});
+        res.status(201).json({message:"Login Successful", token: token})
+
+    } catch (error) {
+        res.status(500).json({message: "Cannot Login"})
     }
 })
 
