f5d1080b3fb3f7fc3430cb2e5f1ff6c5ae5accc3
Aditya Kishor Dahal <toadityadahal@gmail.com>
2024-05-18 20:43:08 +0545

    change: Message Correction for saving user

--- route/userRoute.js
+++ route/userRoute.js
@@ -21,7 +21,7 @@ userRouter.post("/register", async (req, res) => {
         const hashedPassword = await bycrpt.hash(password, 10);
         let user = new userModel({name, email, password: hashedPassword, username});
         await user.save();
-        res.status(200).json({message: `User ${username} Registerd Succesfully`, user})
+        res.status(200).json({message: `User ${username} Registered Succesfully`, user})
         
     } catch (error) {
         res.status(500).json({message: `User with username ${username} cannot be created: `, error})
